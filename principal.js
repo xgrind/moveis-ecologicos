dec123e878a0ff8d1ee86dff14918a6d195e8100
Michael Martins <michaelfm21@gmail.com>
2020-06-12 15:35:34 -0300

    Banner ao abrir pagina

--- principal.js
+++ principal.js
@@ -3,7 +3,7 @@ var bannerAtual = 0;
 
 function trocaBanner() {
     bannerAtual = (bannerAtual + 1) % 2;
-    document.querySelector(’h2#mensagem’).textContent = banners[bannerAtual];
+    document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
 }
 
 setInterval(trocaBanner, 2000);
